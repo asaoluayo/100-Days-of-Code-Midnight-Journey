f7cfdd9d5ef1b4dd428a85b0165531ea1700abaa
SAMZ <asaoluauodipupo2006@gmail.com>
2026-01-26 22:36:57 +0100

    Day 1: Finalized folder structure and fixed file paths

--- day-01-js-basics/script.js
+++ day-01-js-basics/script.js
@@ -1,17 +1,31 @@
 let js = 'Amazing';
 if (js === 'Amazing') alert('Javascript is FUN!');
 
-(40 + 8 + 23 - 18);
 console.log(40 + 8 + 23 - 18);
-let firstName = Ayo;
+
+let firstName = "Ayo";
 console.log(firstName);
-console.log(firstName);
-console.log(firstName);
+
+// VARIABLE NAME CONVENTIONS
+let asaolu_ayo = "AA";
+let $function = 16;
+
+let person = "Jonas";
 let PI = 3.142;
+
 let myfirstJob = "Programmer";
 let mycurrentJob = "Teacher";
 
 let myName = "Samz";
 console.log(myName);
+console.log(myfirstJob);
 
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+console.log(typeof true);
+console.log(javascriptIsFun);
+console.log(typeof 23);
+console.log(typeof 'SAMZ');
 
